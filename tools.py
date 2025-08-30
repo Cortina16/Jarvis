6f538bdb5a1edefaa07c72c1530331383b3bf642
Nightreaper127 <nightreaper125@gmail.com>
2025-08-30 01:02:15 -0700

    Revert "added a requirements.txt"
    This reverts commit 89c84dc47d1f3d01a60013cf1ef51880e0c9c2ba.

--- tools.py
+++ tools.py
@@ -86,7 +86,6 @@ def get_weather(location):
 
 
 #a shitty timer that doesn't work
-#TODO: make this shitter code work
 def start_timer(duration):
     """
     do not use this it  sucks but it's not cool enough to get fixed
